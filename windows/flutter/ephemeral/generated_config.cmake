f0135336190ce380a49b261739295e9beb4adede
Rudranarayan Panda <98372124+Rudra0728@users.noreply.github.com>
2022-12-11 21:46:42 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "R:\\Flutter App Development\\calculator_with_neumorphic_design" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=R:\\Flutter App Development\\calculator_with_neumorphic_design"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=R:\\Flutter App Development\\calculator_with_neumorphic_design\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=R:\\Flutter App Development\\calculator_with_neumorphic_design"
+  "FLUTTER_TARGET=R:\\Flutter App Development\\calculator_with_neumorphic_design\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=R:\\Flutter App Development\\calculator_with_neumorphic_design\\.dart_tool\\package_config.json"
+)
